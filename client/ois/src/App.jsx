80fa024cf1e756d5f44f666e19fe1dd0816f73a3
shivadasari88 <dasarishivasai71@gmail.com>
2024-07-14 16:10:06 +0530

    fresh init

---
+++ client/ois/src/App.jsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+import {Routes, Route} from 'react-router-dom';
+import Navbar from '../src/components/Navbar';
+import Home from '../src/pages/Home'
+import Register from '../src/pages/Register'
+import Login from '../src/pages/Login'
+import axios from 'axios';
+import {Toaster} from 'react-hot-toast';
+import { UserContextProvider } from './contexts/userContext';
+import Dashboard from './pages/Dashboard';
+
+axios.defaults.baseURL= 'http://localhost:3000'
+axios.defaults.withCredentials = true
+function App() {
+  return (
+    <UserContextProvider>
+    <Navbar />
+    <Toaster position='bottom-right' toastOptions={{duration:2000}}/>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+
+    </Routes>
+      
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const cors = require('cors')
+const {mongoose} = require('mongoose')
+const cookieparser = require('cookie-parser')
+
+const app = express();
+
+// Configure CORS middleware (replace '*' with specific origins if needed) it is not secure
+app.use(cors({
+    origin: 'http://localhost:5174', // Replace with your frontend origin
+    credentials: true // Enable credentials
+}));
+
+//databse connection
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log('database connected'))
+.catch((err)=>console.log('database not connected',err))
+
+app.use(express.json())
+app.use(cookieparser());
+app.use(express.urlencoded({extended:false}))
+
+
+app.use('/',require('./routes/authRouter'));
+
+const port = 3000;
+app.listen(port,()=> console.log(`server is listening on port ${port}`))
