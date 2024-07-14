52b4eeb009efa048fcb779342a55002225e488d7
shivadasari88 <dasarishivasai71@gmail.com>
2024-07-14 10:33:14 +0530

    mern setupcomplited

--- client/ois/src/App.jsx
+++ client/ois/src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import {Routes, Route} from 'react-router-dom';
+import Navbar from '../src/components/Navbar';
+import Home from '../src/pages/Home'
+import Register from '../src/pages/Register'
+import Login from '../src/pages/Login'
+import axios from 'axios';
+import {Toaster} from 'react-hot-toast';
+import { UserContextProvider } from './contexts/userContext';
+import Dashboard from './pages/Dashboard';
 
+axios.defaults.baseURL= 'http://localhost:3000'
+axios.defaults.withCredentials = true
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <UserContextProvider>
+    <Navbar />
+    <Toaster position='bottom-right' toastOptions={{duration:2000}}/>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+
+    </Routes>
+      
+    </UserContextProvider>
   )
 }
 
